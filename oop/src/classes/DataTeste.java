a32c3eb2494e4a086213605c5ab327e19656b312
gregorio <gregorio.neto@techsolutio.com>
2023-07-30 19:43:27 -0300

    Construtores

--- oop/src/classes/DataTeste.java
+++ oop/src/classes/DataTeste.java
@@ -4,9 +4,9 @@ public class DataTeste {
 
 	public static void main(String[] args) {
 		Data data = new Data();
-		data.dia = 28;
-		data.mes = 07;
-		data.ano = 2023;
+//		data.dia = 28;
+//		data.mes = 07;
+//		data.ano = 2023;
 		System.out.printf("Data de hoje é %d/%d/%d\n", data.dia,data.mes, data.ano);
 		System.out.println("Data de hoje é " + data.obterDataFormatada());
 
