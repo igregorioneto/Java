ae659e0915c713fb86c91d2abc55907c1779c920
gregorio <gregorio.neto@techsolutio.com>
2023-08-18 08:05:20 -0300

    Studying Streams

--- study-java/src/main/java/streams/filter/Aluno.java
+++ study-java/src/main/java/streams/filter/Aluno.java
@@ -1,11 +1,30 @@
 package streams.filter;
 
+import java.util.Objects;
+
 public class Aluno {
-    final String nome;
-    final double nota;
+    public final String nome;
+    public final double nota;
 
     public Aluno(String nome, double nota) {
         this.nome = nome;
         this.nota = nota;
+    }
+
+    public String toString() {
+        return nome + " tem nota: " + nota;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Double.compare(aluno.nota, nota) == 0 && Objects.equals(nome, aluno.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, nota);
     }
 }
